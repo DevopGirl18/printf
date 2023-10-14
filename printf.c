5bfe76498304c3994fe85b60f5c207372b14bb9e
dansarpong <dansarpong.me@gmail.com>
2023-10-14 16:24:32 +0000

    Add printf file

---
+++ printf.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _printf - prints anything
+ * @format: list of arguments passed to the function
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+    int j, checker, char_count = 0, i = 0;
+	form_spec specifiers[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{NULL, NULL}
+	};
+	va_list args;
+
+	if (!format)
+		return (-1);
+
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			while (specifiers[j])
+			{
+				if (format[i + 1] == specifiers[j].c)
+				{
+					checker = specifiers[j].f(args);
+					if (checker == -1)
+						return (-1);
+					char_count += checker;
+					i++;
+					break;
+				}
+				j++;
+			}
+			if (format[i + 1] != '\0')
+			{
+				char_count += _putchar(format[i]);
+				i++;
+				continue;
+			}
+			else
+				return (-1);
+		}
+		char_count += _putchar(format[i]);
+		i++;
+	}
+}
